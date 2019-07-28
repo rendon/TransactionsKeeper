90d7c141476be431926c45d5da1068540c0bda52
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2019-07-28 11:22:28 -0700

    Read user's input

--- app/src/main/java/mx/letmethink/transactionkeeper/NewTransactionActivity.java
+++ app/src/main/java/mx/letmethink/transactionkeeper/NewTransactionActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class NewTransactionActivity extends AppCompatActivity {
     private static final String TAG = "NewTransaction";
@@ -19,7 +20,20 @@ public class NewTransactionActivity extends AppCompatActivity {
         saveTransactionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "Saving your transaction");
+                TextInputLayout amount = findViewById(R.id.new_transaction_amount);
+                TextInputLayout description = findViewById(R.id.new_transaction_description);
+                String details = String.format(
+                        "Amount = %s Description = %s",
+                        getText(amount), getText(description)
+                );
+                Log.i(TAG, "Saving transaction(" + details + ")");
+            }
+
+            private String getText(final TextInputLayout inputLayout) {
+                if (inputLayout.getEditText() == null) {
+                    return "";
+                }
+                return inputLayout.getEditText().getText().toString();
             }
         });
     }
